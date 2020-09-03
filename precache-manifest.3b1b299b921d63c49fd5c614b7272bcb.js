0fd4e4d949cf4c22ed7353643a70c5d273251204
Kevlar2222 <kevin.satti@hotmail.co.uk>
2020-09-03 16:14:39 +0800

    Updates

---
+++ precache-manifest.3b1b299b921d63c49fd5c614b7272bcb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b263568dfb561c913f2cf7183047848a",
+    "url": "/webcam-effects/index.html"
+  },
+  {
+    "revision": "73f597f6b20dbf6df9ad",
+    "url": "/webcam-effects/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "048c124b3e99e820dc76",
+    "url": "/webcam-effects/static/js/2.4ea69667.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/webcam-effects/static/js/2.4ea69667.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73f597f6b20dbf6df9ad",
+    "url": "/webcam-effects/static/js/main.4892822b.chunk.js"
+  },
+  {
+    "revision": "440650dcb605f0532446",
+    "url": "/webcam-effects/static/js/runtime-main.8c510700.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/webcam-effects/static/media/logo.5d5d9eef.svg"
+  }
+]);
